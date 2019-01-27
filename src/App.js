e744dd89f83956ddf722fb30b78ad9d6c2f5dd08
Zack Woo <zackwoo14@gmail.com>
2019-01-26 19:47:45 -0800

    added New Donations page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,46 @@ const VIEW = {
   DETAIL: 'DETAIL',
 };
 
+const AddNewDonation = ({ handleBack }) => {
+  return (
+    <div className="details-page">
+      <button
+        className="back-button"
+        onClick={() => handleBack()}
+      >
+        Back
+      </button>
+      <div id="new-donation-text">Add new contribution</div>
+      <form>
+        <div id="details-first-row">
+          <span>
+            <label for="contributionField">Contribution type<font color="red">*</font></label>
+            <input type="email" class="form-control" id="contributionField" />
+          </span>
+          <span>
+            <label for="dateField">Date</label>
+            <input type="email" class="form-control" id="dateField" />
+          </span>
+        </div>
+        <div id="details-second-row">
+          <label for="valueField">Value</label>
+          <input type="email" class="form-control" id="valueField" /> 
+        </div>
+        <div id="details-third-row">
+          <label for="noteField">Notes</label>
+          <textarea type="email" class="form-control" id="noteField" /> 
+        </div>
+      </form>
+      <button
+        id="save-button"
+        onClick={null}
+      >
+        Save
+      </button>
+    </div>
+  );
+}
+
 class App extends Component {
 
   constructor(props) {
@@ -66,6 +106,13 @@ class App extends Component {
     document.getElementById('formGroupExampleInput').focus();
   }
 
+  handleBack = () => {
+    this.setState({
+      view: VIEW.SEARCH,
+
+    });
+  }
+
   searchDB = () => {
     let inputText = this.state.searchString;
     window.db
@@ -100,9 +147,7 @@ class App extends Component {
       case VIEW.DETAIL: {
         // this will be the details page component
         return (
-          <div>
-
-          </div>
+          <AddNewDonation handleBack={this.handleBack}/>
         );
       }
 
