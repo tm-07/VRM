344c1e1d368825d4d0769e4cb983e6df880f0fe1
David Sally <davidcsally@gmail.com>
2019-01-27 09:01:16 -0800

    (update) VendorTile to handle search page and detail page views

--- src/components/VendorTile/VendorTile.jsx
+++ src/components/VendorTile/VendorTile.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+const ActionButton = ({ onClick, view, index }) => {
+  const text = (view === 'SEARCH') ? 'Details' : 'Add Donation';
+  const action = (view === 'SEARCH')
+    ? () => onClick(index)
+    : onClick;
+
+  return (
+    <button
+      type="button"
+      className="btn btn-primary details-button"
+      onClick={action}
+    >
+      {text}
+    </button>
+  );
+}
+
 /**
  * Render a Tile containing basic vendor information
  *
  */
-const VendorTile = ({ vendor, index, onClick }) => {
+const VendorTile = ({ vendor, index, onClick, view }) => {
   const {
     name = '',
     address = '',
@@ -13,40 +30,33 @@ const VendorTile = ({ vendor, index, onClick }) => {
     primaryContact = {},
     website = ''
   } = vendor;
-  console.log(index);
   return (
-    <div className={"vendor-container border-"+status + " fade-animation" }>
+    <div className={"vendor-container border-" + status + " fade-animation"}>
       {/* <div className="vendor-img" /> */}
       <div className="row">
         <div className="col-sm-6 vender-header">{name}</div>
-        <div className={"col-sm-6 vendor-status status-"+status }>{status.substring( 0, 1 ).toUpperCase() +status.substring(1)}</div>
+        <div className={"col-sm-6 vendor-status status-" + status}>{status.substring(0, 1).toUpperCase() + status.substring(1)}</div>
       </div>
       <div className="row">
-      <div className="col-sm-6">
-        <div className="row vendor-row">
-          <div className="col-sm-12">
-            <div className="data-header">Address</div>
-            <p>{address}</p>
-            <div className="data-header">Website</div>
-            <p>{website}</p>
+        <div className="col-sm-6">
+          <div className="row vendor-row">
+            <div className="col-sm-12">
+              <div className="data-header">Address</div>
+              <p>{address}</p>
+              <div className="data-header">Website</div>
+              <p>{website}</p>
+            </div>
           </div>
-        </div>
         </div>
         <div className="col-sm-6">
-        <div className="row vendor-row">
-          <div className="col-sm-12">
-          <div className="data-header">Primary Contact</div>
-            <p>Name: {primaryContact.name}<br/>Phone: {primaryContact.phone}<br/>Email: {primaryContact.email}</p>
-            <button
-                        type="button"
-                        className="btn btn-primary details-button"
-                        onClick={onClick}
-                      >
-                        Details
-                      </button>
+          <div className="row vendor-row">
+            <div className="col-sm-12">
+              <div className="data-header">Primary Contact</div>
+              <p>Name: {primaryContact.name}<br />Phone: {primaryContact.phone}<br />Email: {primaryContact.email}</p>
+              <ActionButton onClick={onClick} view={view} index={index} />
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
