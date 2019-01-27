55f54e474bd5c5dc31b3248d9b75c9ea558b4aef
David Sally <davidcsally@gmail.com>
2019-01-27 09:13:18 -0800

    Updates

---
+++ precache-manifest.778f38950282f68465092d1af9a7fc4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7252e1ce55fe48dd052e",
+    "url": "/static/js/main.7252e1ce.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7252e1ce55fe48dd052e",
+    "url": "/static/css/main.71cca571.chunk.css"
+  },
+  {
+    "revision": "5c8f84f6f85019253e6a65fceaf1d401",
+    "url": "/index.html"
+  }
+];
