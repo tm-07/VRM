587dc0f72c9d4f9e5dca86cf4c806743e7ddc089
Zack Woo <zackwoo14@gmail.com>
2019-01-26 17:00:36 -0800

    reduced to one page view

--- src/App.js
+++ src/App.js
@@ -6,29 +6,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      introPage: true,
-      displayPage: false,
       searchString: '',
       data: []
     };
   }
 
   handleSubmit = (e) => {
-    this.setState({
-      introPage: false,
-      displayPage: true
-    });
+    e.preventDefault();
     this.searchDB();
-    this.renderDisplayPage();
-  }
-
-  handleClick = (e) => {
-    this.setState({
-      introPage: true,
-      displayPage: false,
-      searchString: ''
-    });
-    this.renderIntroPage();
   }
 
   handleChange = (e) => {
@@ -59,77 +44,35 @@ class App extends Component {
       });
   }
 
-  renderIntroPage() {
-    return (
-      <>
-        {this.state.introPage ?
-          <div className="intro-page">
-            <p className="form-title">
-              Vendor Relationship Manager
-            </p>
-            <div className="intro-search">
-              <img
-                src={require('./images/Make-A-Wish_small_logo.png')}
-                className="medium-logo"
-                alt="logo"
-                height="36"
-                width="36"
-              />
-
-              <form onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="formGroupExampleInput"
-                    placeholder="Search vendor category..."
-                    onChange={this.handleChange}
-                  />
-                </div>
-              </form>
-            </div>
-          </div>
-          :
-          this.renderDisplayPage()
-        }
-      </>
-    );
-  }
-
-  renderDisplayPage() {
-    return (
-      <>
-        {this.state.displayPage ?
-          <div className="display-page">
-            <div className="small-search">
-              <img
-                src={require('./images/Make-A-Wish_small_logo.png')}
-                className="small-logo"
-                alt="logo"
-                height="30"
-                width="30"
-              />
-              <button
-                type="button"
-                onClick={this.handleClick}
-                className="backButton"
-              >
-                Go back to start page
-              </button>
-            </div>
-            <p>List of vendors related to: {this.state.searchString}</p>
-          </div>
-          :
-          this.renderIntroPage()
-        }
-      </>
-    );
-  }
-
   render() {
     return (
       <main>
-        {this.renderIntroPage()}
+        <div className="main-page">
+          <p className="form-title">
+            Vendor Relationship Manager
+          </p>
+          <div className="intro-search">
+            <img
+              src={require('./images/Make-A-Wish_small_logo.png')}
+              className="medium-logo"
+              alt="logo"
+              height="36"
+              width="36"
+            />
+
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="formGroupExampleInput"
+                  placeholder="Find a vendor..."
+                  onChange={this.handleChange}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
       </main>
     );
   }
