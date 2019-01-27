993e612107f107ed947091434e4909e786223483
David Sally <davidcsally@gmail.com>
2019-01-26 18:56:30 -0800

    (add) onClick to VendorTile to change view to DetailPage

--- src/components/VendorTile/VendorTile.jsx
+++ src/components/VendorTile/VendorTile.jsx
@@ -4,7 +4,7 @@ import React from 'react';
  * Render a Tile containing basic vendor information
  *
  */
-const VendorTile = ({ vendor }) => {
+const VendorTile = ({ vendor, onClick }) => {
   const { name = '',
     address = '',
     updated = '',
@@ -20,19 +20,26 @@ const VendorTile = ({ vendor }) => {
 
   return (
     <div className="vendor-container">
-      {/* <div className="vendor-img" /> */}
       <h3 className="vender-header">{name}</h3>
       <div className="row vendor-row">
         <div className="col-sm-4">
           <h6>Address</h6>
           <p>{address}</p>
-          {/* <p>{address.city}, {address.state} {address.zip}</p> */}
         </div>
-        <div className="col-sm-8">
+        <div className="col-sm-4">
           <h6>Primary Contact</h6>
           <p>Name: {contactName}</p>
           <p>Phone: {phone}</p>
           <p>Email: {email}</p>
+        </div>
+        <div className="col-sm-1">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={onClick}
+          >
+            Details
+          </button>
         </div>
       </div>
     </div>
