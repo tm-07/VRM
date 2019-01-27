36a7f2dcef7e67e3658bb4487bd519d8ea01dbe2
David Sally <davidcsally@gmail.com>
2019-01-26 17:06:07 -0800

    Add VendorTile and some css

---
+++ src/components/VendorTile/VendorTile.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+/**
+ * Render a Tile containing basic vendor information
+ *
+ */
+const VendorTile = ({ vendor }) => {
+  const { name = '',
+    address = '',
+    updated = '',
+    status = '',
+    contact = {},
+  } = vendor;
+
+  const {
+    name: contactName = '',
+    phone = '',
+    email = '',
+  } = contact;
+
+  return (
+    <div className="vendor-container">
+      {/* <div className="vendor-img" /> */}
+      <h3 className="vender-header">{name}</h3>
+      <div className="row vendor-row">
+        <div className="col-sm-4">
+          <h6>Address</h6>
+          <p>{address}</p>
+          {/* <p>{address.city}, {address.state} {address.zip}</p> */}
+        </div>
+        <div className="col-sm-8">
+          <h6>Primary Contact</h6>
+          <p>Name: {contactName}</p>
+          <p>Phone: {phone}</p>
+          <p>Email: {email}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VendorTile;
